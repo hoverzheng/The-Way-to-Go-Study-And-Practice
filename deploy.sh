282b52cd6d4f7fa321741bcdcda0ec56c6f3476c
hoverzheng <zg_hover@126.com>
2018-04-13 07:33:18 +0800

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,28 @@
+#!/bin/bash
+#
+
+## deploy book
+deploy_book()
+{
+	echo "start deploy book ..."
+}
 
 
-git commit -m "updates"
-git remote add origin https://github.com/hoverzheng/The-Way-to-Go-Study-And-Practice.git
-git push -u origin master
+## deploy master(default)
+deploy_master()
+{
+	git commit -m "updates"
+	git remote add origin https://github.com/hoverzheng/The-Way-to-Go-Study-And-Practice.git
+	git push -u origin master
+}
+
+
+# main
+main()
+{
+	echo "main"
+}
+
+
+# run 
+main
